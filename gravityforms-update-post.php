ec49f0efbadf88a28bbe2c7e3a5c7140f0816304
jcow <jcow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-19 03:33:24 +0000

    1.2.22 radio cat fix and undeclared post_id
    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-post-updates/trunk@1183920 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gravityforms-update-post.php
+++ gravityforms-update-post.php
@@ -8,7 +8,7 @@
  * Plugin Name:       Gravity Forms: Post Updates
  * Plugin URI:        https://wordpress.org/plugins/gravity-forms-post-updates/
  * Description:       Allow Gravity Forms to update post content and the meta data associated with a post.
- * Version:           1.2.20
+ * Version:           1.2.22
  * Author:            Jupitercow
  * Author URI:        http://Jupitercow.com/
  * Contributer:       ekaj
@@ -40,7 +40,7 @@ class gform_update_post
 	 * @since 	1.2
 	 * @var 	string
 	 */
-	const VERSION = '1.2.20';
+	const VERSION = '1.2.21';
 
 	/**
 	 * Settings
@@ -111,9 +111,9 @@ class gform_update_post
 	{
 		$require_link = ( isset($atts['require_link']) || in_array('require_link', $atts) ) ? true : false;
 
+		$post_id = false;
 		if ( $require_link )
 		{
-			$post_id = false;
 			$request_key = apply_filters(self::PREFIX . '/request_id', self::$settings['request_id']);
 			if (! empty($_REQUEST[$request_key]) ) {
 				$post_id = $_REQUEST[$request_key];
@@ -1071,7 +1071,13 @@ class gform_update_post
 	 */
 	public static function return_category_field_value( $value, $field )
 	{
-		return (! empty(self::$settings['cat_value']) ) ? self::$settings['cat_value'] : $value;
+		$result = (! empty(self::$settings['cat_value']) ) ? self::$settings['cat_value'] : $value;
+
+		if ( 'radio' == $field->inputType && isset($result[0]) ) {
+			return $result[0];
+		} else {
+			return $result;
+		}
 	}
 	public static function return_taxonomy_field_value( $value, $field )
 	{
